58f263047f73b954ec11d2472dbde5d8963bb78e
Beacb <102221998+Beacb@users.noreply.github.com>
2022-04-13 13:51:15 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,3 @@
-let laranjas;
-
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(0)
@@ -8,9 +6,23 @@ function setup() {
 
 function draw() {
 
-	stroke(230);
-	strokeWeight(2);
-	noFill();
+	background(255, 255, 255);
+
+	if(mouseY>=height/2 && mouseX<=width/2)  
+    fill(0, 74, 173);
+	
+    else if(mouseY<height/2 && mouseX<=width/2) 
+    fill(232,36,36);
+  	
+    else if(mouseY<height/2 && mouseX>width/2) 
+    fill(0,128,55); 
+
+	else{
+		 noFill();
+	}
+
+	stroke(0);
+	strokeWeight(1);
 	circle(width / 2, height / 2, height / 4);
 
 
